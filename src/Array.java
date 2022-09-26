4013b0e93c4450c1bc774bdde7321512e5d9e56e
DimetraLis <ekaterinaumetskaya@yandex.ru>
2022-09-26 03:38:53 +0300

    first commit

---
+++ src/Array.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class Array {
+    private final int n=10;
+    private final Scanner sc=new Scanner(System.in);
+    private final int[] array= new int[n];
+    private int max=Integer.MIN_VALUE;
+    private int min=Integer.MAX_VALUE;
+    private float avg =0;
+
+    /** Чтение массива с клавиатуры*/
+    public void read_array(){
+        System.out.println("Enter array:");
+        for (int i=0;i<array.length;i++){
+            System.out.print("Array[" + i + "]=");
+            array[i]=sc.nextInt();
+        }
+    }
+    /** Отображение массива на экране*/
+    public void print_array(){
+        System.out.println("Your array is:");
+        for (int i=0;i<array.length;i++){
+            System.out.print(array[i] + " ");
+        }
+    }
+    /** Сортировка массива по возрастанию*/
+    public void sort_up_array(){
+        for (int j=0; j< array.length;j++) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    array[i] = array[i] + array[i + 1];
+                    array[i + 1] = array[i] - array[i + 1];
+                    array[i] = array[i] - array[i + 1];
+                }
+            }
+        }
+    }
+    /** Сортировка массива по убыванию*/
+    public void sort_down_array(){
+        for (int j=0; j< array.length;j++) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] < array[i + 1]) {
+                    array[i] = array[i] + array[i + 1];
+                    array[i + 1] = array[i] - array[i + 1];
+                    array[i] = array[i] - array[i + 1];
+                }
+            }
+        }
+    }
+    /** Нахождение минимального элемента массива*/
+    public void find_min(){
+        for (int i=0;i<array.length;i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        System.out.print("Min:" + min);
+    }
+    /** Нахождение максимального элемента массива*/
+    public void find_max(){
+        for (int i=0;i<array.length;i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        System.out.print("Max:" + max);
+    }
+    /** Нахождение среднего значения элементов массива*/
+    public void find_avg(){
+        for (int i=0;i<array.length;i++) {
+            avg+=array[i];
+        }
+        avg=avg/ array.length;
+        System.out.print("Average:" + avg);
+    }
+    /** Нахождение простых чисел среди элементов массива*/
+    public void prime_num(){
+        System.out.println("1 and 2 are already prime numbers, so they don't count");
+        System.out.println("Prime numbers in your array:");
+        for(int i = 0; i <array.length;i ++){
+            boolean isPrime = true;
+            if (array[i]>2) {
+                for (int j = 2; j < array[i]; j++) {
+                    if (array[i] % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) {
+                    System.out.print(array[i] + " ");
+                }
+            }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void Menu() {
+        System.out.println("Menu:");
+        System.out.print("""
+                1-Enter array
+                2-Print array
+                3-Find max element in array
+                4-Find min element in array
+                5-Sort up array
+                6-Sort down array
+                7-Find average num of array
+                8-find prime num in array
+                9-help menu
+                0-exit""");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Array A = new Array();
+        A.read_array();
+        Menu();
+        boolean fl=true;
+        while (fl) {
+            System.out.println("\n\nchoose case:");
+            int ch = sc.nextInt();
+            switch (ch) {
+                case 1 -> A.read_array();
+                case 2 -> A.print_array();
+                case 3 -> A.find_max();
+                case 4 -> A.find_min();
+                case 5 -> {
+                    System.out.println("Sort up array");
+                    A.sort_up_array();
+                    A.print_array();
+                }
+                case 6 -> {
+                    System.out.println("Sort down array");
+                    A.sort_down_array();
+                    A.print_array();
+                }
+                case 7 -> A.find_avg();
+                case 8 -> A.prime_num();
+                case 9 -> Menu();
+                case 0 -> {
+                    fl = false;
+                    System.out.print("Goodbye!");
+                }
+                default -> System.out.println("No such an option");
+            }
+        }
+    }
+}
